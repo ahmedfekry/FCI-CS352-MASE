1fecd29b5a4d8cec941afa4d9c7ad3f3a8dd7b6a
ahmedfekry <ahmedfikry78@gmail.com>
2015-04-07 22:40:00 +0200

    Fekry Adding getNoticifaction to the controller

--- src/com/FCI/SWE/Controller/NotificationController.java
+++ src/com/FCI/SWE/Controller/NotificationController.java
@@ -2,6 +2,7 @@ package com.FCI.SWE.Controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -15,6 +16,10 @@ import org.glassfish.jersey.server.mvc.Viewable;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import com.FCI.SWE.Models.Notification;
+import com.google.appengine.labs.repackaged.org.json.JSONArray;
+import com.google.appengine.repackaged.com.google.gson.JsonArray;
 // connection 
 // link
 //String serviceUrl = "http://fci-swe-apps.appspot.com/rest/RegistrationService";
@@ -76,5 +81,32 @@ public class NotificationController {
 	}
 	/////////////////////////////////////////////////////////////////////////////////
 	
-	
+	@POST
+	@Path("/getNotifications")
+	public Response viewAllNotification(@FormParam("username")String username,@FormParam("password") String password){
+		String serviceUrl = "http://localhost:8888/rest/getUnseenMessages";
+		String urlParameters = "username=" + username + "&password=" + password;
+		String retJson = Connection.connect(serviceUrl, urlParameters, "POST",
+				"application/x-www-form-urlencoded;charset=UTF-8");
+		JSONParser parser = new JSONParser();
+		Vector<Notification> notification = new Vector<>();
+		try {
+			JSONArray Array = (JSONArray)parser.parse(retJson);
+			
+			// System.out.println(retJson);
+		//	obj = parser.parse(retJson);
+			//JSONObject object = (JSONObject) obj;
+			//if (object.get("Status").equals("OK"))
+				// map.put("message", "Message has been sent Successfully");
+			
+			//else
+				// map.put("message", (String) object.get("Status"));
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return Response.ok(new Viewable("/jsp/messageResponse")).build();
+	}
 }
